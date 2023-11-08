bf2754a036936f328d105ce62147613066bd501a
Samuel Vanmeenen Bachelard <svanmeen@z1r4p5.42lyon.fr>
2023-11-08 12:06:46 +0100

    Fixed some Harl/replace args

--- Module1/ex05/Harl.cpp
+++ Module1/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:41:34 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/09/13 21:04:10 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/11/08 12:05:26 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,35 @@ Harl::~Harl()
 	std::cout << "Harl destroyed" << std::endl;
 }
 
-void	Harl::_debug()
+void Harl::_debug()
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
-void	Harl::_info()
+void Harl::_info()
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
-void	Harl::_warning()
+void Harl::_warning()
 {
 	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void	Harl::_error()
+void Harl::_error()
 {
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
-void	Harl::complain(std::string level)
+void Harl::complain(std::string level)
 {
 	fptr fptrs[4] = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	for (int i = 0; i < 4; i++)
 	{
 		if (levels[i] == level)
-			(this->*fptrs[i])();
+			return ((this->*fptrs[i])());
 	}
+	if (level != "EXIT" && !(std::cin.eof()))
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
--- Module1/ex05/main.cpp
+++ Module1/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 20:56:19 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/09/13 21:03:32 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/11/08 12:04:03 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 #include <iostream>
 #include <string>
 
-int main( void ) {
+int main(void)
+{
 
-	std::string	input;
-	Harl		harl;
+	std::string input;
+	Harl harl;
 
-	std::cout << "Enter a complain: ";
-	std::cin >> input;
-	harl.complain(input);
-	while (input.compare("EXIT"))
+	while (!(std::cin.eof()) && input.compare("EXIT"))
 	{
-		std::cout << "Enter a level: ";
+		std::cout << "Enter a level: " << std::endl;
 		std::cin >> input;
 		harl.complain(input);
 	}
