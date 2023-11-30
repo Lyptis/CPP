5a7209e9fa8c717e4559e2c3cb485b5fc1a8c3d1
Samuel Vanmeenen Bachelard <svanmeen@z4r4p1.42lyon.fr>
2023-11-30 10:33:34 +0100

    Harl buffer fix

--- Module1/ex05/main.cpp
+++ Module1/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 20:56:19 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/11/08 12:04:03 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/11/30 10:29:29 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int main(void)
 		std::cout << "Enter a level: " << std::endl;
 		std::cin >> input;
 		harl.complain(input);
+		input.clear();
 	}
 	return (0);
 }
