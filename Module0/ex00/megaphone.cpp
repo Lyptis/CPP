9166d50e1cb21001d1c3b1fbf7ab49eb0244683a
Samuel Vanmeenen Bachelard <svanmeen@z4r4p1.42lyon.fr>
2023-11-30 12:02:55 +0100

    Added strings

--- Module0/ex00/megaphone.cpp
+++ Module0/ex00/megaphone.cpp
@@ -6,16 +6,24 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:21:31 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/07/26 14:05:09 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:01:39 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void	print_upper(std::string str)
+{
+	for (int i = 0; i < (int)str.size(); i++)
+		std::cout << (char)toupper(str[i]);
+}
+
+
 int	main(int argc, char **argv)
 {
 	int	i = 1;
 	int	j = 0;
+	std::string	str;
 
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
@@ -23,10 +31,11 @@ int	main(int argc, char **argv)
 	{
 		j = 0;
 		while (argv[i][j]) {
-			std::cout << (char)toupper(argv[i][j]);
+			str += argv[i][j];
 			j++;
 		}
 		i++;
 	}
+	print_upper(str);
 	std::cout << std::endl;
 }
