2aaac6f0df2e813016d340c26ad72d9bdc6a605b
Lyptis <samuel.vanmeenen@gmail.com>
2024-04-08 15:16:20 +0200

    M9

---
+++ Module9/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 11:26:46 by svanmeen          #+#    #+#             */
+/*   Updated: 2024/04/08 14:38:51 by svanmeen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <math.h>
+
+class FileException : public std::exception {
+	public:
+		const char *what() const throw();
+};
+
+class BadInputException : public std::exception {
+	public:
+		const char *what() const throw();
+};
+
+class NegNumberException : public std::exception {
+	public:
+		const char *what() const throw();
+};
+
+class NumberTooBigException : public std::exception {
+	public:
+		const char *what() const throw();
+};
+
+
+std::map<std::string, float> parsedata();
+void parseinput(char *file, std::map<std::string, float> tab);
+
+#endif
